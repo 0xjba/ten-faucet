5eeed4ca2fa48411db279eac9f639fb09886df26
Jobin Ayathil <0xjba@Macbook-0xJba.local>
2023-12-04 13:18:10 +0530

    Basic Faucet Implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [walletAddress, setWalletAddress] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+      e.preventDefault();
+      setMessage('Loading...'); // Display a loading message
+      try {
+          const res = await fetch('http://sepolia-testnet-faucet.uksouth.azurecontainer.io/fund/eth', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ Address: walletAddress })
+          });
+          const data = await res.json();
+          setMessage('Success: ' + JSON.stringify(data)); // Display the success message
+      } catch (error) {
+          setMessage('Error: ' + error.message); // Display the error message
+      }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>Wallet Faucet</h1>
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                value={walletAddress}
+                onChange={(e) => setWalletAddress(e.target.value)}
+                placeholder="Enter Wallet Address"
+                required
+            />
+            <button type="submit">Submit</button>
+        </form>
+        <div>{message}</div>
     </div>
-  );
+);
 }
 
 export default App;
